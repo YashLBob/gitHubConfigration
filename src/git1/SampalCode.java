0a3859fd4bddcf734290835a9f499963487c9f11
LOCKENEUS <Yash123linganwar@gmail.com>
2025-01-29 20:41:15 +0530

    First commit

---
+++ src/git1/SampalCode.java
@@ -0,0 +1,19 @@
+package git1;
+
+public class SampalCode {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		int a,b,c ;
+		
+		a= 100;
+		b= 200;
+		
+		c= a+b;
+		System.out.println(c);
+		
+	}
+
+}
