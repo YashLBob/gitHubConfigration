6e29ada412c1a974d26c794455f73f11ee10db96
LOCKENEUS <Yash123linganwar@gmail.com>
2025-01-29 20:46:45 +0530

    new calculation added

--- src/git1/SampalCode.java
+++ src/git1/SampalCode.java
@@ -6,13 +6,16 @@ public class SampalCode {
 	{
 		// TODO Auto-generated method stub
 		
-		int a,b,c ;
+		int a,b,c,d;
 		
 		a= 100;
 		b= 200;
 		
 		c= a+b;
 		System.out.println(c);
+		 d = a*b;
+		 System.out.println(d);
+		
 		
 	}
 
